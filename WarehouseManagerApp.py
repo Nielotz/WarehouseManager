2977e337d8fb050ce84282517a93e3438ebc6f6b
Nielotz <mati2806@poczta.fm>
2022-06-04 16:47:52 +0200

    Make API return storages from db

--- WarehouseManagerApp.py
+++ WarehouseManagerApp.py
@@ -1,15 +1,27 @@
 from flask import Flask, jsonify
 from flask import render_template
+import bleach
+
+import database
+from database import table
 
 app = Flask(__name__)
+
+db = database.init(app)
 
 
 class RestApi:
     class Get:
         @staticmethod
-        @app.route("api/storages", methods=["GET"])
+        @app.route("/api/storages", methods=["GET"])
         def storages():
-            return jsonify({"storage": ("DummyStorage1", "DummyStorage2", "DummyStorage3")})
+            storages_from_db: [table.Storage, ] = table.Storage.query.all()
+            storages_names = map(lambda storage: storage.name, storages_from_db)
+            bleached_storages_names = map(
+                lambda name: bleach.clean(text=name, tags=(), attributes={}, protocols=()),
+                storages_names
+            )
+            return jsonify(tuple(bleached_storages_names))
 
         @staticmethod
         @app.route("/api/storage/<storage>/containers", methods=["GET"])
