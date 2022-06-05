d4c36a111c28712cd8ab1250e29d74c03f02a3c9
Nielotz <mati2806@poczta.fm>
2022-06-05 16:29:01 +0200

    Add repr to tables

--- database/table.py
+++ database/table.py
@@ -6,20 +6,31 @@ from . import db
 class Storage(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.Text, default='DefaultColumnName')
+
     # user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<{type(self).__name__}: id={self.id}, name={self.name}>"
 
 
 class Container(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.Text, default='DefaultContainerName')
     storage_id = db.Column(db.Integer, db.ForeignKey("storage.id"), nullable=False)
+
     # created_by = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<{type(self).__name__}: id={self.id}, name={self.name}, storage_id={self.storage_id}>"
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.Text, default='DefaultColumnName')
     barcode = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<{type(self).__name__}: id={self.id}, name={self.name}, barcode={self.barcode}>"
 
 
 class ItemHistory(db.Model):
@@ -28,7 +39,11 @@ class ItemHistory(db.Model):
     amount = db.Column(db.Integer)
     # amount_unit = db.Column(db.Int,  REFERENCES amount_unit,
     changed = db.Column(db.DateTime, default=datetime.now().isoformat())
+
     # prize = db.Column(db.Integer,  -- Stores real prize * 100 to avoid float point inaccuracy.
     # shop_id = db.Column(db.Integer,  REFERENCES shop(shop_id),
     # changed_by = db.Column(db.Integer,  REFERENCES userdata(user_id)
 
+    def __repr__(self):
+        return f"<{type(self).__name__}: " \
+               f"id={self.id}, item_id={self.item_id}, amount={self.amount}, changed={self.changed}>"
