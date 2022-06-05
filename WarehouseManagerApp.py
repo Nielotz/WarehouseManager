6ccd33f4c6b9da6fd5193354590255f2f2b113b7
Nielotz <mati2806@poczta.fm>
2022-06-05 21:58:01 +0200

    Make api return objects from database instead of names

--- WarehouseManagerApp.py
+++ WarehouseManagerApp.py
@@ -1,6 +1,6 @@
+import bleach
 from flask import Flask, jsonify
 from flask import render_template
-import bleach
 
 import database
 from database import table
@@ -10,28 +10,39 @@ app = Flask(__name__)
 db = database.init(app)
 
 
+def extract_and_bleach(items: [], sub_item_getter: callable) -> [str, ]:
+    return [bleach.clean(text=sub_item_getter(item), tags=(), attributes={}, protocols=()) for item in items]
+
+
 class RestApi:
     class Get:
         @staticmethod
-        @app.route("/api/storages_names", methods=["GET"])
-        def storages_names():
-            storages_from_db: [table.Storage, ] = table.Storage.query.all()
-            storages_names = map(lambda storage: storage.name, storages_from_db)
-            bleached_storages_names = map(
-                lambda name: bleach.clean(text=name, tags=(), attributes={}, protocols=()),
-                storages_names
-            )
-            return jsonify({"names": tuple(bleached_storages_names)})
+        @app.route("/api/storages", methods=["GET"])
+        def storages():
+            storages_data: [table.Storage, ] = table.Storage.query.all()  # TODO: Optimize - query only names.
+            # TODO: Security - add sanitization.
+            # TODO: Security - add fields whitelist.
+            return jsonify([sd.to_dict() for sd in storages_data])
 
         @staticmethod
-        @app.route("/api/storage/<storage>/containers_names", methods=["GET"])
-        def containers_names(storage):
-            return jsonify({"names": (f"Storage: {storage}", "DummyContainers2", "DummyContainers3")})
+        @app.route("/api/storages/<int:storage_id>/containers", methods=["GET"])
+        def containers(storage_id: int):
+            # TODO: Optimize - query only names.
+            storage_data: table.Storage = table.Storage.query.filter_by(id=storage_id).first()
+
+            if storage_data is None:
+                return jsonify({})
+
+            containers_data: [table.Container, ] = table.Container.query \
+                .join(table.Storage, table.Container.storage_id == table.Storage.id) \
+                .filter(storage_data.id == table.Container.storage_id).all()
+
+            return jsonify([cd.to_dict() for cd in containers_data])
 
         @staticmethod
-        @app.route("/api/storage/<storage>/container/<container>/items_names", methods=["GET"])
-        def items_names(storage, container):
-            return jsonify({"names": (f"Storage: {storage}", f"Container: {container}", "DummyItem3")})
+        @app.route("/api/storages/<int:storage_id>/containers/<int:container_id>/items", methods=["GET"])
+        def items(storage_id: int, container_id: int):
+            return jsonify({"names": (f"Storage: {storage_id}", f"Container: {container_id}", "DummyItem3")})
 
 
 @app.route("/")
